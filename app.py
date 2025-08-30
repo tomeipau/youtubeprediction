96bbd6918f66c1d5a298be6aae3fc53162237c37
tomeipau <149239277+tomeipau@users.noreply.github.com>
2025-08-30 13:15:59 +0800

    Update app.py

--- app.py
+++ app.py
@@ -3,6 +3,7 @@ import pandas as pd
 import joblib
 import plotly.express as px
 import plotly.graph_objects as go
+from plotly.subplots import make_subplots  
 
 # Page layout
 st.set_page_config(layout="wide")
