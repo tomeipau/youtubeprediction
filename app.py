9c8292975029c209a754cb269f697c614f52cf9e
tomeipau <149239277+tomeipau@users.noreply.github.com>
2025-09-05 18:15:36 +0800

    Update app.py

--- app.py
+++ app.py
@@ -64,7 +64,7 @@ def show_analysis():
         st.dataframe(df_raw.head(5))
         st.markdown("""
     Data is extracted from Youtube API V3 and downloaded from https://www.kaggle.com/datasets/rsrishav/youtube-trending-video-dataset?select=US_youtube_trending_data.csv. 
-    Data consists of records of nideo title, channel title, publish time, tags, views, likes and dislikes, description, and comment count that is extracted daily
+    The dataset consists of 268,787 entries with 12 attributes and contains data from the year 2020-2024 in the US region. Target variable will be fixed as view_count and likes attribute to narrow down the study.
     """)
 
          #--Processed Dataset (Encodings & LLM Embedded) Overview
